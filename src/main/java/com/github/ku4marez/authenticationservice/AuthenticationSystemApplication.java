9bfbfe919727d092b550373386ec88df3e97d21a
Anton Kucha <anton.kucha@aionys.com>
2025-01-04 20:58:56 +0100

    Fix failing tests

--- src/main/java/com/github/ku4marez/authenticationservice/AuthenticationSystemApplication.java
+++ src/main/java/com/github/ku4marez/authenticationservice/AuthenticationSystemApplication.java
@@ -1,9 +1,12 @@
 package com.github.ku4marez.authenticationservice;
 
+import com.github.ku4marez.authenticationservice.repository.custom.CustomSearchRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(repositoryBaseClass = CustomSearchRepositoryImpl.class)
 public class AuthenticationSystemApplication {
 
 	public static void main(String[] args) {
