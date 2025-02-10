8fc3cf5cd9925fb3a5668447bc2b8d63042a3688
Anton Kucha <anton.kucha@aionys.com>
2025-02-11 00:09:52 +0100

    Audit entity

---
+++ src/main/java/com/github/ku4marez/authenticationservice/configuration/AppConfiguration.java
@@ -0,0 +1,29 @@
+package com.github.ku4marez.authenticationservice.configuration;
+
+import com.github.ku4marez.commonlibraries.util.AuditorAwareUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Configuration
+@EnableMongoAuditing
+public class AppConfiguration {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new AuditorAwareUtil(this::getCurrentUser);
+    }
+
+    private String getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated()) {
+            return authentication.getName();
+        }
+
+        return "system";
+    }
+}
