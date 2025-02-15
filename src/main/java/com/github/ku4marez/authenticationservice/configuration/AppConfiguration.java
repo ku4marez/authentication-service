af68b7c9fe3ec6f75ae5a05c76bb6aa2e57ca30a
Anton Kucha <anton.kucha@aionys.com>
2025-02-15 21:25:23 +0100

    Fix application startup issues

--- src/main/java/com/github/ku4marez/authenticationservice/configuration/AppConfiguration.java
+++ src/main/java/com/github/ku4marez/authenticationservice/configuration/AppConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
-@EnableMongoAuditing
+@EnableMongoAuditing(auditorAwareRef = "auditorProvider")
 public class AppConfiguration {
 
     @Bean
